2a9cee4ac347c07fd1c292220b740228e086fc8f
Vedant Nikam <vedantnikam500@gmail.com>
2026-08-19 10:43:10 +0530

    first reacttttttttttt

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <br />
+
+      <Link to="/products">Products</Link>
+      <br />
+    </nav>
+  );
+}
