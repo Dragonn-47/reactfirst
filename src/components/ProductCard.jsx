13c0469428b40f4f2a0e223de8ff65eccd1bc4a1
Vedant Nikam <vedantnikam500@gmail.com>
2026-08-31 10:05:32 +0530

    add cardcontext.jsx

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,28 @@
 import "./ProductCard.css";
+import React, { useContext } from "react";
+import { CartContext } from "../context/CartContext";
 
-function ProductCard({ title, price, image}) {
+function ProductCard({ title, price, image }) {
+    const { dispatch } = useContext(CartContext);
+
     return (
         <div className="card">
-    <img className="card" src={image} alt={title} />
-    <h3 className="title">{title}</h3>
-    <p className="price">${price}</p>
-</div>
-    )
+            <img className="card-img" src={image} alt={title} />
+            <h3 className="title">{title}</h3>
+            <p className="price">${price}</p>
+
+            <button
+                onClick={() =>
+                    dispatch({
+                        type: "ADD_TO_CART",
+                        payload: { title, price, image },
+                    })
+                }
+            >
+                Add to Cart
+            </button>
+        </div>
+    );
 }
 
 export default ProductCard;
